b8c2235b6e4667672e304570babec3834c34bbf4
David Ramirez <r.davidsaid06@gmail.com>
2025-02-11 22:30:44 -0800

    Movimiento derecha-izquierda

--- voidglow/src/gameobjects/Player.js
+++ voidglow/src/gameobjects/Player.js
@@ -68,14 +68,17 @@ export class Player extends Physics.Arcade.Image {
     }
 
     move(direction) {
-        if(this.state === "can_move") {
+        if (this.state === "can_move") {
             if (direction === "up" && this.y - 10 > 0) {
                 this.y -= 5;
-                this.updatePropulsionFire();
             } else if (direction === "down" && this.y + 75 < this.scene.scale.height) {
                 this.y += 5;
-                this.updatePropulsionFire();
+            } else if (direction === "left" && this.x - 10 > 0) {
+                this.x -= 5;
+            } else if (direction === "right" && this.x + 75 < this.scene.scale.width) {
+                this.x += 5;
             }
+            this.updatePropulsionFire();
         }
     }
 
